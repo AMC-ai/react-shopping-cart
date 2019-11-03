10f712b8e08891bd19748f646a05a23ca35bcfb8
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-11-03 17:38:00 -0500

    added contexts folder and files

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
+	//notice there are two states (products availabe and cart for keepin track of shopper items)
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
@@ -15,6 +16,7 @@ function App() {
 		// add the given item to the cart
 	};
 
+	//notice 3 components used 
 	return (
 		<div className="App">
 			<Navigation cart={cart} />
---
+++ src/contexts/ProductContext.js
@@ -0,0 +1,7 @@
+import createContext from 'react';
+
+const ProductContext = () => {
+
+}
+
+export default ProductContext;
